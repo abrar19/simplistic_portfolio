81260abe538e6194c1a9a0b8b2fc81212b89f82f
Abrar Haque <abm.abrar@gmail.com>
2023-04-19 06:05:23 +0200

    contact done with email js

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,10 +53,54 @@ let swiper = new Swiper(".mySwiper", {
   });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiper2 = new Swiper(".mySwiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactProject = document.getElementById('contact-project'),
+      contactMessage = document.getElementById('contact-message');
 
+      const sendEmail = (e) => {
+        e.preventDefault();
+        // Check if the field has a value
+        if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+          // Add and remove color
+          contactMessage.classList.remove('color-blue');
+          contactMessage.classList.add('color-red');
+
+          // Show message	
+          contactMessage.textContent = 'Fill all the input fields 📓'
+        }else{
+          // serviceID - templateID - #form - publicKey
+          emailjs.sendForm('service_r7t2ccl', 'template_uae1j2b', '#contact-form', 'WWuJueMMDUeu0SAYr')
+          .then(() => {
+            // Show message and add color
+            contactMessage.classList.add('color-blue')
+            contactMessage.textContent = 'Message sent 🟩'
+
+            //Remove message after 5 seconds
+            setTimeout(()=>{
+              contactMessage.textContent = '';
+            }, 5000)
+          });
+        }
+      } 
+      
+      // Remove message after five seconds
+    
+      // Send Error
+        //OOPS! SOMETHING HAS FAILED...
+    
+    // To clear the input field
+
+      contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
